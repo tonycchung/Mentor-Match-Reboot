1f3a3d529d751645ee328f594358f826a1919bc3
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-23 17:14:04 -0700

    add pundit gem

--- Gemfile
+++ Gemfile
@@ -1,8 +1,15 @@
 source 'https://rubygems.org'
 
 gem 'coffee-rails', '~> 4.0.0'
+gem 'devise'
+gem 'figaro'
+gem 'haml'
 gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
+gem 'minitest-rails-capybara', group: :test
+gem 'omniauth-github'
+gem 'omniauth-linkedin'
+gem 'pundit'
 gem 'rails', '4.1.1'
 gem 'sass-rails', '~> 4.0.3'
 gem 'sdoc', '~> 0.4.0', group: :doc
@@ -10,9 +17,3 @@ gem 'spring', group: :development
 gem 'sqlite3'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
-gem 'devise'
-gem 'haml'
-gem "minitest-rails-capybara", group: :test
-gem 'omniauth-github'
-gem 'omniauth-linkedin'
-gem 'figaro'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623183704) do
+ActiveRecord::Schema.define(version: 20140623214207) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20140623183704) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
