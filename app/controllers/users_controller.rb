09df0aa5da42f3d1a9ae386441ff06694dc5547b
Tony Chung <anthonyuchung@gmail.com>
2014-06-24 11:27:05 -0700

    Add static_pages controller and static splash page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-
-  def splash
-  end
   # GET /users
   # GET /users.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#splash'
+  root 'static_pages#splash'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
