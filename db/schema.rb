0c37012f4e8a173f4f2c607538daad159ead21ee
Tony Chung <anthonyuchung@gmail.com>
2014-06-24 11:07:47 -0700

    Add company column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623214207) do
+ActiveRecord::Schema.define(version: 20140624180643) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140623214207) do
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
+    t.string   "company"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
