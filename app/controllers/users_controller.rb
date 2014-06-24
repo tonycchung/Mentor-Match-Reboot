f6bf464f5c1a7b30d577caa62423d4b3169917c8
Tony Chung <anthonyuchung@gmail.com>
2014-06-24 14:24:13 -0700

    Setup custom user dashboard page for mentor or mentee.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def dashboard
+    @user = User.find(params[:id])
+  end
+
   # GET /users
   # GET /users.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get 'users/:id/dashboard' => 'users#dashboard', as: :dashboard
   # You can have the root of your site routed with "root"
   root 'static_pages#splash'
 
