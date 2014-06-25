a7f20cad04fd2943730af87d14613cac751e9a39
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-25 10:18:20 -0700

    create friendship(mentorship) controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'friendship/create'
+
+  get 'friendship/destroy'
+
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", registrations: "registrations"}
     resources :users
   # The priority is based upon order of creation: first created -> highest priority.
