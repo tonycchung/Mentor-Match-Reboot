9990ea546beef67763632a56b2cb20e6fd83d1e7
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-26 14:44:39 -0700

    validations on friendship model to assure that multiple frienship with the same person cannot be created and that you cannot create a friendship with yourself

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,12 +2,7 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User"
   validate :legit_friendship?
-
-private
-
-  def already_friend?
-    current_user.friendships.where(:friend_id => user.id).where(:user_id => current_user.id).empty?
-  end
+  validates :user_id, uniqueness: {scope: :friend_id}
 
   def legit_friendship?
     friend_id != user_id
