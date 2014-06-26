0f6336a524e9d4c4d9ba2e396b4f044aae062ebb
bwstud <bwstudwell@gmail.com>
2014-06-26 09:18:08 -0700

    add fontawesome for icon font support

--- app/assets/javascripts/intro.js
+++ app/assets/javascripts/intro.js
@@ -1,6 +1,20 @@
 $(function() {
     $('.linkage1').click(function() {
-      alert("yo");
-      $.scrollTo('#two', 800, {easing:'swing'} );
+      $.scrollTo('#_2', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('.linkage2').click(function() {
+      $.scrollTo('#_3', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('.linkage3').click(function() {
+      $.scrollTo('#_4', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('.linkage4').click(function() {
+      $.scrollTo('#_5', 800, {easing:'swing'} );
     });
 });
