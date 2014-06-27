f3b2b7cfee9e124b7f62df556dc1ac7b79219501
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-26 21:49:58 -0700

    add tests for sign in and dashboard and fix prior tests

--- test/features/mentorship_relationships_test.rb
+++ test/features/mentorship_relationships_test.rb
@@ -18,7 +18,7 @@ feature "A mentor and mentee can interact with eachother" do
   scenario "As a mentee, I want to be able to bookmark a mentor for future reference" do
     sign_in_mentee
     visit users_path
-    click_on "View Profile"
+    first(:link, "View Profile").click
     click_on "Add to favorites"
     visit dashboard_path
     click_on "View Favorites"
