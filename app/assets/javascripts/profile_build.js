b68e4d5220a7a1c4324f4e383c726ee6dabf917e
bwstud <bwstudwell@gmail.com>
2014-06-27 03:46:22 -0700

    style profile view

--- app/assets/javascripts/profile_build.js
+++ app/assets/javascripts/profile_build.js
@@ -1,23 +1,52 @@
 $(function() {
-    $('.next').click(function() {
-        // Calling a function in case you want to expand upon this.
-        next();
+    $('#next_1').click(function() {
+      $.scrollTo('#_2', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('#next_2').click(function() {
+      $.scrollTo('#_3', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('#next_3').click(function() {
+      $.scrollTo('#_4', 800, {easing:'swing'} );
+    });
+});
+$(function() {
+    $('#next_4').click(function() {
+      $.scrollTo('#_5', 800, {easing:'swing'} );
     });
 });
 
-function next() {
-    alert("Success");
-    if ($('this').hasClass('show')) {
-        $('this').removeClass('show');
-        $('this').addClass('hidden');
-        if ($('this').hasClass('1')) {
-          $('2').removeClass('hidden');
-        } else if ($('this').hasClass('2')) {
-          $('3').removeClass('hidden');
-        } else if ($('this').hasClass('3')) {
-          $('4').removeClass('hidden');
-        } else {
-          $('5').removeClass('hidden');
-        }
-      }
-    }
+// $(function(){
+//   if($("#_1:in-viewport").$(function(){
+//     $("ul > li").slice(1).css("background-color", "red"));
+//   }
+// })
+//     $('.next').click(function() {
+//       alert("booya");
+//       if($(this).parents().hasClass('show'));
+//       $(this).parents().removeClass('show');
+//       $(this).parents().addClass('hidden');
+//       next();
+//         // Calling a function in case you want to expand upon this.
+//     });
+// });
+
+// $("next")function()
+// function next() {
+//     alert("Success");
+//     if ($(this).hasClass('show')) {
+//         $(this).removeClass('show');
+//         $(this).addClass('hidden');
+//         } else if ($(this).hasClass('_1')) {
+//           $('_2').removeClass('hidden');
+//         } else if ($(this).hasClass('_2')) {
+//           $('_3').removeClass('hidden');
+//         } else if ($(this).hasClass('_3')) {
+//           $('_4').removeClass('hidden');
+//         } else {
+//           $('_5').removeClass('hidden');
+//         };
+//       }
