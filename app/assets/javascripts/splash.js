4267156a16b8c3b48cac5ce6734a33edccdad201
bwstud <bwstudwell@gmail.com>
2014-07-12 12:30:32 -0700

    add fade-in the hero copy on mousemove and window.scroll

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -4,3 +4,17 @@ $(".findMentor").mouseenter(function(){
 $(".beMentore").mouseenter(function(){
   $(".beMentor").text("Lead the Way");
 });
+
+// var fade_in = function(){
+
+// }
+
+window.onscroll = function(){
+  document.getElementById('student').style.opacity='1';
+  document.getElementById('mentor').style.opacity='1';
+}
+
+window.onmousemove  = function(){
+  document.getElementById('student').style.opacity='1';
+  document.getElementById('mentor').style.opacity='1';
+}
