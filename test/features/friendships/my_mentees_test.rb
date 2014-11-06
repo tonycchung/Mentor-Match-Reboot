38039abeb18b6892f08532bce9470edd5f2cb7b7
Philip Nguyen <supertaru@gmail.com>
2014-11-06 11:44:18 -0800

    my mentor and my mentee page

--- test/features/friendships/my_mentees_test.rb
+++ test/features/friendships/my_mentees_test.rb
@@ -7,6 +7,16 @@ feature 'As a mentor, I want to see mentees who are connected to me' do
     click_on 'Request Mentor'
     click_on 'Sign Out'
 
+    sign_in_mentor
+    first(:link, 'Accept').click
+    visit user_path users(:mentee_user_3)
+    click_on 'Request Mentee'
+    click_on 'Sign Out'
+
+    sign_in_mentee :mentee_user_3
+    first(:link, 'Accept').click
+    click_on 'Sign Out'
+
     sign_in_mentee :mentee_user_2
     click_on 'Sign Out'
     sign_in_mentee :mentee_user_2
@@ -15,16 +25,54 @@ feature 'As a mentor, I want to see mentees who are connected to me' do
     click_on 'Sign Out'
 
     sign_in_mentor
-    first(:link, 'Accept').click
+  end
+
+  scenario 'I should see My Mentees link on the dashboard' do
     visit root_path
-    first(:link, 'Accept').click
+    page.must_have_link 'My Mentees'
+  end
+
+  scenario 'I should see My Mentees link from the Browse path' do
+    visit users_path
+    page.must_have_link 'My Mentees'
+  end
+
+  scenario 'I should see My Mentees link from the friendships path' do
+    visit friendships_path
+    page.must_have_link 'My Mentees'
   end
 
   scenario 'If I click on My Mentees, I should see my mentees' do
     click_on 'My Mentees'
 
-    page.must_have_content users(:mentee_user).first_name
-    page.must_have_content users(:mentee_user_2).first_name
-    page.wont_have_content users(:mentee_user_3).first_name
+    page.must_have_content users(:mentee_user).fullname
+    page.wont_have_content users(:mentee_user_2).fullname
+    page.must_have_content users(:mentee_user_3).fullname
+    page.wont_have_content users(:mentee_user_4).fullname
+  end
+end
+
+feature 'As a mentee, I should be able to view my connected mentors' do
+  before do
+    sign_in_mentee
+    visit user_path users(:mentor_user)
+    click_on 'Request Mentor'
+    click_on 'Sign Out'
+
+    sign_in_mentor
+    first(:link, 'Accept').click
+    visit user_path users(:mentee_user_3)
+    click_on 'Request Mentee'
+    click_on 'Sign Out'
+
+    sign_in_mentee
+  end
+
+  scenario 'I should see my mentors after clicking My Mentors link' do
+
+    click_on 'My Mentors'
+    save_and_open_page
+    page.must_have_content users(:mentor_user).first_name
+    page.must_have_content users(:mentor_user).last_name
   end
 end
