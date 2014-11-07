c0890560e09a5870b2c3058c542a7dc63581aed3
Philip Nguyen <supertaru@gmail.com>
2014-11-06 18:22:14 -0800

    Add :authenticate_user! and comment out codefellows_course

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :update_sanitized_params, if: :devise_controller?
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   include Pundit
 
