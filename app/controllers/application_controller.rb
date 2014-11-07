57848faaa762845414ee25f1c448d3f4e332db84
Philip Nguyen <supertaru@gmail.com>
2014-11-06 22:36:30 -0800

    remove useless sanitize params method in ApplicationsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :update_sanitized_params, if: :devise_controller?
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   include Pundit
@@ -9,10 +8,6 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-
-  def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:role, :email, :password)}
-  end
 
   def user_not_authorized
     flash[:error] = "You are not authorized to perform this action."
