7de02035077ae21294a841607cf5e4c60b31c2a5
Tony Chung <anthonyuchung@gmail.com>
2014-11-13 00:11:18 -0800

    Remove faker from development in Gemfile. Remove local redis server.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
-REDIS = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
